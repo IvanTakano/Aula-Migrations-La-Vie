eca3173e7a080ce82ef9b4e1ba71dc6999d9fe8a
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-07 20:27:54 -0300

    Seeds e caminho da pasta

--- src/infrastructure/database/seeders/seeders/20220607232149-psicologos.js
+++ src/infrastructure/database/seeders/seeders/20220607232149-psicologos.js
@@ -3,42 +3,40 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('pacientes', [
+    await queryInterface.bulkInsert('psicologos', [
       {
         nome: 'John Doe',
         email: 'John@gmail.com',
-        idade: '2022/01/01',
+        senha: '12345',
+        apresentacao: 'John Doe',
         createdAt: new Date(),
         updatedAt: new Date()
-
       },
       {
-        nome: 'Junior Lima',
-        email: 'Junior@gmail.com',
-        idade: '2022/01/01',
-        createdAt: new Date(),
-        updatedAt: new Date()
-
-      },
-      {
-        nome: 'Marocs Doe',
-        email: 'Marocs@gmail.com',
-        idade: '2022/01/01',
-        createdAt: new Date(),
-        updatedAt: new Date()
-
-      },
-      {
-        nome: 'Tomas Doe',
+        nome: 'Tomas Benso',
         email: 'Tomas@gmail.com',
-        idade: '2022/01/01',
+        senha: '123456',
+        apresentacao: 'Tomas Benso',
         createdAt: new Date(),
         updatedAt: new Date()
-
       },
-
+      {
+        nome: 'Nina Nunes',
+        email: 'NunesNina@gmail.com',
+        senha: '54612',
+        apresentacao: 'Nina Nunes',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        nome: 'Carlos Fisher',
+        email: 'Carlos@gmail.com',
+        senha: '54ds58ad4a',
+        apresentacao: 'Carlos Fisher',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ], {});
-
   },
 
   async down(queryInterface, Sequelize) {
